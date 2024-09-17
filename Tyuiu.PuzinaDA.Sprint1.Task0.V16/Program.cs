f5f017ca4e8bc95ce0a799d4f3db212cb4eb36fc
Tyuiu.puzinada@outlook.com <dariapuzina7b@gmail.com>
2024-09-17 23:02:54 +0500

    .

--- Tyuiu.PuzinaDA.Sprint1.Task0.V16/Program.cs
+++ Tyuiu.PuzinaDA.Sprint1.Task0.V16/Program.cs
@@ -27,7 +27,9 @@ namespace Tyuiu.PuzinaDA.Sprint1.Task0.V16
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
+
             Console.WriteLine(ds.Calculate());
+
             Console.ReadLine();
         }
     }
